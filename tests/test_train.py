486a4bba4c7d17df74f94013ced48a69acab4efe
tvartiainen <46996237+tvartiainen@users.noreply.github.com>
2023-08-11 16:05:10 +0300

    Add new user in dataset (#2)
    * Update first.csv
    
    * Update train.py
    
    fixing blank lines from train.py
    
    * Update once again train.py
    
    * install pytest
    
    * fix assertion error

--- tests/test_train.py
+++ tests/test_train.py
@@ -19,4 +19,4 @@ def test_csvs_creates_dataframe():
     current_directory = os.path.dirname(os.path.abspath(__file__))
     datasets_directory = os.path.join(current_directory, 'datasets')
     result = get_csvs_df(datasets_directory)
-    assert len(result) == 20
+    assert len(result) == 21
