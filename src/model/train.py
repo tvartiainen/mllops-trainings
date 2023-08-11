fd5433034d1877c8967259b1184847f9accee2cc
tvartiainen <46996237+tvartiainen@users.noreply.github.com>
2023-08-11 13:00:07 +0300

    Bug fixes in train.py model

--- src/model/train.py
+++ src/model/train.py
@@ -3,7 +3,7 @@
 import argparse
 import glob
 import os
-
+import mlfow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
@@ -18,7 +18,7 @@ def main(args):
     df = get_csvs_df(args.training_data)
 
     # split data
-    X_train, X_test, y_train, y_test = split_data(df)
+    X_train, X_test, y_train, y_test = split_data(df) # noqa: F821
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
@@ -60,6 +60,7 @@ def parse_args():
 # run script
 if __name__ == "__main__":
     # add space in logs
+
     print("\n\n")
     print("*" * 60)
 
